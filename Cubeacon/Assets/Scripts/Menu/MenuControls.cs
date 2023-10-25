03c24185bd124316ea3a9fe13081c937fff00e39
Denis <109104246+Denis0014@users.noreply.github.com>
2023-10-25 20:43:13 +0300

    add menu scene

---
+++ Cubeacon/Assets/Scripts/Menu/MenuControls.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitPressed()
+    {
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+    }
+}
