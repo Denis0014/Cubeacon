1928a302c272d5be54ea460454d30550e3cd58b9
kirillBaev <kiryabaev00@mail.ru>
2023-11-09 20:56:22 +0300

    Background and menu sounds

---
+++ Cubeacon/Assets/Scripts/MenuFX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFX : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+}
