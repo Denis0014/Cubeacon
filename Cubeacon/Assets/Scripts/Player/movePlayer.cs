760de921ad0225937c4fbd15c9926aa492445b05
D2eKA <145915244+D2eKA@users.noreply.github.com>
2023-11-15 16:02:33 +0300

    Add pause menu

--- Cubeacon/Assets/Scripts/Player/movePlayer.cs
+++ Cubeacon/Assets/Scripts/Player/movePlayer.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class movePlayer : InteractiveObject
 {
-    public float speed = 1; //Нам это точно нужно?
-
+    public float speed = 1; //пїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ?
+    public bool PauseMenu;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (PauseMenu != true)
         {
-            tryToMove(new Vector2(0, 1));
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                tryToMove(new Vector2(0, 1));
+            }
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                tryToMove(new Vector2(-1, 0));
+            }
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                tryToMove(new Vector2(0, -1));
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                tryToMove(new Vector2(1, 0));
+            }
         }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            tryToMove(new Vector2(-1, 0));
-        }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            tryToMove(new Vector2(0, -1));
-        }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            tryToMove(new Vector2(1, 0));
-        }
+
     }
 }
