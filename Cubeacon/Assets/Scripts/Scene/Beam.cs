056641edba8dbf1fab428d84be32507145919d0d
C1R9A7W2D <DrSpore2@gmail.com>
2023-11-22 22:23:58 +0300

    Update Beam.cs
    Просто немного оптимизировал код

--- Cubeacon/Assets/Scripts/Scene/Beam.cs
+++ Cubeacon/Assets/Scripts/Scene/Beam.cs
@@ -16,6 +16,77 @@ public class Beam : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    
+
+    void Update()
+    {
+        transform.Translate(xSpeed, ySpeed, 0);
+        if (isBlocked())
+            Destroy(transform.gameObject);
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+            Destroy(transform.gameObject);
+
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Interactive"));
+        if (hit.collider == null)
+            return;
+
+        switch(hit.collider.gameObject.tag)
+        {
+            case "MirrorRU":
+                if (xSpeed < 0 && ySpeed == 0)
+                {
+                    ySpeed = -xSpeed;
+                    xSpeed = 0;
+                }
+                else if (xSpeed == 0 && ySpeed < 0)
+                {
+                    xSpeed = -ySpeed;
+                    ySpeed = 0;
+                }
+                break;
+
+            case "MirrorLU":
+                if (xSpeed > 0 && ySpeed == 0)
+                {
+                    ySpeed = xSpeed;
+                    xSpeed = 0;
+                }
+                else if (xSpeed == 0 && ySpeed < 0)
+                {
+                    xSpeed = ySpeed;
+                    ySpeed = 0;
+                }
+                break;
+
+            case "MirrorRD":
+                if (xSpeed < 0 && ySpeed == 0)
+                {
+                    ySpeed = xSpeed;
+                    xSpeed = 0;
+                }
+                else if (xSpeed == 0 && ySpeed > 0)
+                {
+                    xSpeed = ySpeed;
+                    ySpeed = 0;
+                }
+                break;
+
+            case "MirrorLD":
+                if (xSpeed > 0 && ySpeed == 0)
+                {
+                    ySpeed = -xSpeed;
+                    xSpeed = 0;
+                }
+                else if (xSpeed == 0 && ySpeed > 0)
+                {
+                    xSpeed = -ySpeed;
+                    ySpeed = 0;
+                }
+                break;
+        }
+    }
+
     private bool isBlocked()
     {
         RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Wall"));
@@ -26,111 +97,5 @@ public class Beam : MonoBehaviour
         if (hit.collider == null)
             hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Light wall"));
         return hit.collider != null && hit.collider.gameObject.tag == "not a mirror";
-    }
-
-    private bool isMirrorRU()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Interactive"));
-        return hit.collider != null && hit.collider.gameObject.tag == "MirrorRU";
-    }
-
-    private bool isMirrorLU()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Interactive"));
-        return hit.collider != null && hit.collider.gameObject.tag == "MirrorLU";
-    }
-
-    private bool isMirrorRD()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Interactive"));
-        return hit.collider != null && hit.collider.gameObject.tag == "MirrorRD";
-    }
-
-    private bool isMirrorLD()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Interactive"));
-        return hit.collider != null && hit.collider.gameObject.tag == "MirrorLD";
-    }
-
-    void Update()
-    {
-        transform.Translate(xSpeed, ySpeed, 0);
-        if (isBlocked())
-            Destroy(transform.gameObject);
-        lifeTime -= Time.deltaTime;
-        if (lifeTime <= 0)
-            Destroy(transform.gameObject);
-        if (isMirrorRU())
-        {
-            //Debug.Log("isMirrorRU");
-            if (xSpeed < 0 && ySpeed == 0)
-            {
-                ySpeed = -xSpeed;
-                xSpeed = 0;
-                //Debug.Log("TurnRU");
-                return;
-            }
-            if (xSpeed == 0 && ySpeed < 0)
-            {
-                xSpeed = -ySpeed;
-                ySpeed = 0;
-                //Debug.Log("TurnUR");
-                return;
-            }
-        }
-        if (isMirrorLU())
-        {
-            //Debug.Log("isMirrorLU");
-            if (xSpeed > 0 && ySpeed == 0)
-            {
-                ySpeed = xSpeed;
-                xSpeed = 0;
-                //Debug.Log("TurnLU");
-                return;
-            }
-            if (xSpeed == 0 && ySpeed < 0)
-            {
-                xSpeed = ySpeed;
-                ySpeed = 0;
-                //Debug.Log("TurnUL");
-                return;
-            }
-        }
-        if (isMirrorRD())
-        {
-            //Debug.Log("isMirrorRD");
-            if (xSpeed < 0 && ySpeed == 0)
-            {
-                ySpeed = xSpeed;
-                xSpeed = 0;
-                //Debug.Log("TurnRD");
-                return;
-            }
-            if (xSpeed == 0 && ySpeed > 0)
-            {
-                xSpeed = ySpeed;
-                ySpeed = 0;
-                //Debug.Log("TurnDR");
-                return;
-            }
-        }
-        if (isMirrorLD())
-        {
-            //Debug.Log("isMirrorLD");
-            if (xSpeed > 0 && ySpeed == 0)
-            {
-                ySpeed = -xSpeed;
-                xSpeed = 0;
-                //Debug.Log("TurnLD");
-                return;
-            }
-            if (xSpeed == 0 && ySpeed > 0)
-            {
-                xSpeed = -ySpeed;
-                ySpeed = 0;
-                //Debug.Log("TurnDL");
-                return;
-            }
-        }
     }
 }
