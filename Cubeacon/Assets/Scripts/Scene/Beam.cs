36ea5479cfa5cb42e3c21321c2d244102ff41aa9
kirillBaev <kiryabaev00@mail.ru>
2023-11-26 22:26:20 +0300

    Finish

--- Cubeacon/Assets/Scripts/Scene/Beam.cs
+++ Cubeacon/Assets/Scripts/Scene/Beam.cs
@@ -17,7 +17,6 @@ public class Beam : MonoBehaviour
     }
 
     
-
     void Update()
     {
         transform.Translate(xSpeed, ySpeed, 0);
@@ -27,11 +26,13 @@ public class Beam : MonoBehaviour
         if (lifeTime <= 0)
             Destroy(transform.gameObject);
 
+        Finish();
+        
         RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Interactive"));
         if (hit.collider == null)
             return;
 
-        switch(hit.collider.gameObject.tag)
+        switch (hit.collider.gameObject.tag)
         {
             case "MirrorRU":
                 if (xSpeed < 0 && ySpeed == 0)
@@ -85,6 +86,7 @@ public class Beam : MonoBehaviour
                 }
                 break;
         }
+        
     }
 
     private bool isBlocked()
@@ -97,5 +99,15 @@ public class Beam : MonoBehaviour
         if (hit.collider == null)
             hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Light wall"));
         return hit.collider != null && hit.collider.gameObject.tag == "not a mirror";
+    }
+
+    private void Finish()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, new Vector3(xSpeed, ySpeed, 0), 0.05f, LayerMask.GetMask("Wall"));
+        if (hit.collider != null && hit.collider.gameObject.tag == "Finish")
+        {
+            hit.collider.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            Destroy(transform.gameObject);
+        }
     }
 }
