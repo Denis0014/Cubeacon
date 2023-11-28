511604627267f7204d2a7c4d0f428bcf5680fa02
Denis <109104246+Denis0014@users.noreply.github.com>
2023-11-28 22:07:17 +0300

    Player animation
    Co-Authored-By: thenotoriousgrant <147095148+thenotoriousgrant@users.noreply.github.com>

--- Cubeacon/Assets/Scripts/Player/movePlayer.cs
+++ Cubeacon/Assets/Scripts/Player/movePlayer.cs
@@ -4,26 +4,40 @@ using UnityEngine;
 
 public class movePlayer : InteractiveObject
 {
+    public Animator animator;
     public bool PauseMenu;
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
-        if (PauseMenu != true)
+        if (!PauseMenu)
         {
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
-                tryToMove(new Vector2(0, 1));
+                if (tryToMove(new Vector2(0, 1)))
+                    animator.SetTrigger("Up");
             }
             if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                tryToMove(new Vector2(-1, 0));
+                if (tryToMove(new Vector2(-1, 0)))
+                    animator.SetTrigger("Go");
+                sr.flipX = true;
             }
             if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             {
                 tryToMove(new Vector2(0, -1));
+                animator.SetTrigger("Down");
             }
             if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
-                tryToMove(new Vector2(1, 0));
+                if (tryToMove(new Vector2(1, 0)))
+                    animator.SetTrigger("Go");
+                sr.flipX = false;
             }
         }
 
