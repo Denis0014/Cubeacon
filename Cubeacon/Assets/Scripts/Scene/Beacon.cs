28a929fa9a6bc991c46737082afe4b6dbe8fd6ee
C1R9A7W2D <DrSpore2@gmail.com>
2023-12-29 22:45:29 +0300

    Rename reflect script and clean it a bit

--- Cubeacon/Assets/Scripts/Scene/Beacon.cs
+++ Cubeacon/Assets/Scripts/Scene/Beacon.cs
@@ -23,8 +23,7 @@ public class Beacon : MonoBehaviour
 
     protected void EmitRay()
     {
-        RaycastHit2D[] hits = GetAllHits();
-        RaycastHit2D hit = GetMinDist(hits);
+        RaycastHit2D hit = GetClosestObjectHit();
 
         if (hit.collider != null)
         {
@@ -36,7 +35,7 @@ public class Beacon : MonoBehaviour
 
             if (hit.collider.gameObject.tag == "Mirror")
             {
-                Reflect reflect = hit.collider.gameObject.GetComponent<Reflect>();
+                Reflective reflect = hit.collider.gameObject.GetComponent<Reflective>();
                 SpriteRenderer spriteRenderer = hit.collider.gameObject.GetComponent<SpriteRenderer>();
 
                 reflect.isReflect = true;
@@ -57,21 +56,13 @@ public class Beacon : MonoBehaviour
             l.SetPositions(new Vector3[2] { transform.position, transform.position + new Vector3(xSpeed, ySpeed, 0) * 1000 });
         }
     }
-
-    private RaycastHit2D[] GetAllHits()
+    
+    private RaycastHit2D GetClosestObjectHit()
     {
-        return new RaycastHit2D[4] { GetHit("Wall"), GetHit("Interactive"), GetHit("Player"), GetHit("Light wall") };
-    }
-
-    private RaycastHit2D GetHit(string mask)
-    {
-        return Physics2D.Raycast(transform.position + new Vector3(Math.Sign(xSpeed) * 0.5f, Math.Sign(ySpeed) * 0.5f, 0), new Vector3(xSpeed, ySpeed, 0), Mathf.Infinity, LayerMask.GetMask(mask));
-    }
-
-    private RaycastHit2D GetMinDist(RaycastHit2D[] hits)
-    {
-        float min = float.MaxValue;
-        RaycastHit2D result = Physics2D.Raycast(transform.position, new Vector3(xSpeed, ySpeed, 0), Mathf.Infinity, 99);
+        RaycastHit2D[] hits = GetAllHits();
+    
+        RaycastHit2D result = hits[0];
+        float min = result.distance;
 
         foreach (var x in hits)
             if (x.collider != null && x.distance < min)
@@ -81,5 +72,15 @@ public class Beacon : MonoBehaviour
             }
 
         return result;
+    }
+    
+    private RaycastHit2D[] GetAllHits()
+    {
+        return new RaycastHit2D[4] { GetHit("Wall"), GetHit("Interactive"), GetHit("Player"), GetHit("Light wall") };
+    }
+
+    private RaycastHit2D GetHit(string mask)
+    {
+        return Physics2D.Raycast(transform.position + new Vector3(Math.Sign(xSpeed) * 0.5f, Math.Sign(ySpeed) * 0.5f, 0), new Vector3(xSpeed, ySpeed, 0), Mathf.Infinity, LayerMask.GetMask(mask));
     }
 }
