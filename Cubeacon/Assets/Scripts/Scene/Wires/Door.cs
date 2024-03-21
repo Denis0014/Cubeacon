0b5efa9ba3cfe0a1140d73af74a7dd994897d979
Denis <109104246+Denis0014@users.noreply.github.com>
2024-03-21 21:00:47 +0300

    door fix

--- Cubeacon/Assets/Scripts/Scene/Wires/Door.cs
+++ Cubeacon/Assets/Scripts/Scene/Wires/Door.cs
@@ -9,7 +9,7 @@ public class Door : WiringSys
     private Vector3 pos0;
     override protected void Start()
     {
-        pos1 = gameObject.transform.position + new Vector3(0f, 0.4f, 0f);
+        pos1 = gameObject.transform.position + new Vector3(0f, 1f, 0f);
         pos0 = gameObject.transform.position;
         base.Start();
     }
@@ -19,16 +19,12 @@ public class Door : WiringSys
         if (activated == signal_type)
         {
             gameObject.layer = 11;
-            gameObject.GetComponent<SpriteRenderer>().size = new Vector2(0.3f, 0.3f);
-            gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0, 0);
             gameObject.transform.position = pos1;
             gameObject.tag = "Untagged";
         }
         else
         {
             gameObject.layer = 8;
-            gameObject.GetComponent<SpriteRenderer>().size = new Vector2(0.3f, 1f);
-            gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.3f, 1f);
             gameObject.transform.position = pos0;
             gameObject.tag = "Blocks movement";
         }
