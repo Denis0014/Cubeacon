c75639d98d99dae69ce3eadf217e6aa1d6015827
Denis <109104246+Denis0014@users.noreply.github.com>
2024-04-26 00:16:20 +0300

    Base undo

---
+++ Cubeacon/Assets/Scripts/Scene/Undo.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Undo : MonoBehaviour
+{
+    public Stack<Dictionary<GameObject, Vector3>> history;
+
+    public Undo()
+    {
+        history = new Stack<Dictionary<GameObject, Vector3>>();
+    }
+
+    private static Undo instance;
+    public static Undo Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<Undo>();
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning(this + ": походу " + instance.gameObject + "ведёт историю за меня!");
+        }
+        instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (history.Count > 0)
+            {
+                var objects = history.Pop();
+                foreach (var obj in objects.Keys)
+                    obj.transform.position = objects[obj];
+            }
+        }
+    }
+}
