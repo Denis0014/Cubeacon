3ac10db5b9ceffcfc6c2704da7c0ea605c32cb77
Denis <109104246+Denis0014@users.noreply.github.com>
2024-05-19 17:30:49 +0300

    Revert "Merge pull request #43 from Denis0014/bug-fixes"
    This reverts commit 3c92f7e1aac347aeee8ba4619e108c3af2e70189, reversing
    changes made to e09d2a93d7e128f131e7790217b92f21570d2cf8.

--- Cubeacon/Assets/Scripts/Scene/Wires/Door.cs
+++ Cubeacon/Assets/Scripts/Scene/Wires/Door.cs
@@ -23,11 +23,13 @@ public class Door : WiringSys
         {
             gameObject.layer = 11;
             gameObject.transform.position = pos1;
+            gameObject.tag = "Untagged";
         }
         else
         {
             gameObject.layer = 8;
             gameObject.transform.position = pos0;
+            gameObject.tag = "Blocks movement";
         }
     }
 
