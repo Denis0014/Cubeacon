8b75ff105c515c56316615f9b3b14fd6048cb5b1
Denis <109104246+Denis0014@users.noreply.github.com>
2024-05-19 17:32:21 +0300

    Reapply "step 2"
    This reverts commit 85d70dbcb78c2a2cb432e3f1c6f8d8b28dddcc5a.

--- Cubeacon/Assets/Scripts/Scene/Interactive/InteractiveObject.cs
+++ Cubeacon/Assets/Scripts/Scene/Interactive/InteractiveObject.cs
@@ -1,11 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class InteractiveObject : MonoBehaviour
 {
